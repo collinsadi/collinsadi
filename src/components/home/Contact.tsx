60f9c099e6f40d9f22960cfa71a1ec355fbea4c9
collinsadi <collinsadi20@gmail.com>
2024-12-24 13:07:06 +0100

    updated contact info

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -7,7 +7,12 @@ export const Contact = () => {
         </div>
 
         <div>
-          <h3 className="md:text-6xl text-4xl font-bold italic">hello@collinsadi.me</h3>
+          <a href="mailto:hello@collinsadi.xyz" className="w-fit">
+            <h3 className="md:text-6xl text-4xl font-bold italic w-fit">
+              hello@collinsadi.xyz
+            </h3>
+          </a>
+
           <p className="my-4">Let's Build Something Amazing together</p>
         </div>
       </div>
